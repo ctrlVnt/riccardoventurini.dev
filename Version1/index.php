01d76923f4aeb3b3e23f1915a6d151743b3b05a7
liveprog <Giordanobruno227@gmail.com>
2022-12-15 19:28:03 +0100

    aggiornata v1 e v3

---
+++ Version1/index.php
@@ -0,0 +1,30 @@
+<?php
+
+function strip_crlf($string)
+{
+    return str_replace("\r\n", "", $string);
+}
+
+if (! empty($_POST["send"])) {
+    $name = $_POST["userName"];
+    $email = $_POST["userEmail"];
+    $subject = $_POST["subject"];
+    $content = $_POST["content"];
+
+    $toEmail = "admin@phppot_samples.com";
+    // CRLF Injection attack protection
+    $name = strip_crlf($name);
+    $email = strip_crlf($email);
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "The email address is invalid.";
+    } else {
+        // appending \r\n at the end of mailheaders for end
+        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+        if (mail($toEmail, $subject, $content, $mailHeaders)) {
+            $message = "Your contact information is received successfully.";
+            $type = "success";
+        }
+    }
+}
+require_once "contact-view.php";
+?>
