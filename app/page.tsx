be4e661dfc99863fa7ef0118eb7c096addc82873
riccardo <giordanobruno227@gmail.com>
2024-03-05 13:32:11 +0100

    pulse animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
-import { Pointer } from 'lucide-react';
 
 const navigation = [
   { name: "Projects", href: "/projects" },
@@ -13,11 +12,11 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-10">
-          {navigation.map((item) => (
+          {navigation.map((item, index) => (
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-sm duration-500 text-zinc-400 hover:text-zinc-300 animate-pulse" style={{ animationDelay: `${index * 300}ms` }}
             >
               {item.name}
             </Link>
@@ -35,7 +34,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-400 ">
           Web & Mobile developer
         </h2>
       </div>
