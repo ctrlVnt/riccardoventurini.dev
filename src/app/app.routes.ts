e443c98a195203dbe71d0aa3e4a449053245d0d7
riccardo <giordanobruno227@gmail.com>
2024-08-18 16:57:25 +0200

    added privacy and edit footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FlashappComponent } from './privacy/flashapp/flashapp.component';
+import { RiccardoventuriniComponent } from './privacy/riccardoventurini/riccardoventurini.component';
+import { RytmComponent } from './privacy/rytm/rytm.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'privacy/flashapp', component: FlashappComponent },
+    { path: 'privacy/riccardoventurini', component: RiccardoventuriniComponent },
+    { path: 'privacy/rytm', component: RytmComponent },
+];
