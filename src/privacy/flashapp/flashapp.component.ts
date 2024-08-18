1d29bf1714b73e27524fd370a78d9b3397c9e380
riccardo <giordanobruno227@gmail.com>
2024-08-18 16:20:32 +0200

    policy privacy

---
+++ src/privacy/flashapp/flashapp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flashapp',
+  standalone: true,
+  imports: [],
+  templateUrl: './flashapp.component.html',
+  styleUrl: './flashapp.component.css'
+})
+export class FlashappComponent {
+
+}
---
+++ src/privacy/rytm/rytm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rytm',
+  standalone: true,
+  imports: [],
+  templateUrl: './rytm.component.html',
+  styleUrl: './rytm.component.css'
+})
+export class RytmComponent {
+
+}
