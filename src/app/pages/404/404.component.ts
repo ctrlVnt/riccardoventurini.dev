f89d6791d162c074180067799d88503b6b00b027
ctrlVnt <gordanobruno227@gmail.com>
2025-02-16 19:54:42 +0100

    improved route logic + added 404 page

---
+++ src/app/pages/404/404.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-404',
+  standalone: true,
+  imports: [],
+  templateUrl: './404.component.html',
+  styleUrl: './404.component.css'
+})
+export class NotFoundComponent {
+  constructor(private router: Router) {}
+  
+  goHome() {
+    this.router.navigate(['/']);
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -26,9 +25,9 @@ import { ServicesComponent } from "./services/services.component";
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatListModule, MatInputModule, MatFormFieldModule, MatGridListModule, MatDividerModule, MatButtonModule, MatIconModule, MatSidenavModule, MatCardModule, MatMenuModule, PortfolioComponent, ContactComponent, FooterComponent, HeaderComponent, MenuComponent, AboutComponent, ServicesComponent],
+  imports: [CommonModule, MatListModule, MatInputModule, MatFormFieldModule, MatGridListModule, MatDividerModule, MatButtonModule, MatIconModule, MatSidenavModule, MatCardModule, MatMenuModule, PortfolioComponent, ContactComponent, FooterComponent, HeaderComponent, MenuComponent, AboutComponent, ServicesComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
   animations: [
@@ -40,7 +39,7 @@ import { ServicesComponent } from "./services/services.component";
   ],
 })
 
-export class AppComponent{
+export class Homecomponent{
   
   scrol = 0;
 
